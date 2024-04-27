3686f2e96baf7930419c5c393eb0bbeddf83ea95
prabodhani1234 <83935882+prabodhani1234@users.noreply.github.com>
2024-04-27 20:17:05 +0530

    Design buttons in Right side

--- client/monarch/src/App.js
+++ client/monarch/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import logo from "./assets/logo.png";
 import img1 from "./assets/img1.png";
+import { VscDebugRestart } from "react-icons/vsc";
 
 function App() {
   return (
@@ -9,7 +10,6 @@ function App() {
         {/* Right */}
         <div className="md:w-5/6 h-screen bg-gradient-to-r from-[#BBBBBB] via-white to-[#BBBBBB]">
           <div className="flex flex-col md:flex-row justify-between">
-
             {/*Logo */}
             <div className="max-w-sm py-7 px-7 mx-auto">
               <img className="w-24 md:w-auto" src={logo} alt="" />
@@ -22,8 +22,23 @@ function App() {
 
             {/*Main Text */}
             <div className="py-20 px-20 flex flex-col items-end">
+              <div className="py-2">
                 <h1 className="text-5xl font-bold">$110</h1>
               </div>
+              <div className=" py-2">
+                <button className="btn bg-black px-8 py-3 font-semibold text-white uppercase ">
+                  continue
+                </button>
+              </div>
+
+              <div className="py-2">
+                <button className="btn bg-white px-3 py-1 border border-black text-xs flex items-center">
+                  {" "}
+                  <VscDebugRestart />
+                  <span> Reset Change</span>
+                </button>
+              </div>
+            </div>
           </div>
         </div>
 
